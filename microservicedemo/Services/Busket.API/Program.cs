36466f15c6c1ae07312b12354884bde7d31bb783
Mahedi Hasan Niloy <niloy.oct@gmail.com>
2024-05-13 17:29:29 +0600

    start configure redis with api

--- microservicedemo/Services/Busket.API/Program.cs
+++ microservicedemo/Services/Busket.API/Program.cs
@@ -2,6 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddStackExchangeRedisCache(options => options.Configuration = "localhost:6379");
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
